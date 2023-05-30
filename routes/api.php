62216acabe835cbbe8c4335932e59bd068175891
unknown <frifqi50@gmail.com>
2023-05-30 07:25:25 +0700

    coba absen

--- routes/api.php
+++ routes/api.php
@@ -27,3 +27,7 @@ Route::group(['prefix' => 'auth'], function () {
         Route::post('/password/reset', 'Api\Auth\PasswordController@reset')
         ->middleware('auth:sanctum');
 });
+
+Route::group(['middlewar' => 'auth::sanctum'], function(){
+    Route::post('attendance', 'Api\AttendanceController@store');
+});
