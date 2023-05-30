83c4c369ba23bd28327f857a4797e53aaa58c805
unknown <frifqi50@gmail.com>
2023-05-31 03:21:18 +0700

    9

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('user', 'UserController');
+Route::resource('attendance', 'AttendanceController')->only(['index', 'show']);
+
