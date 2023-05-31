7eb8dccc519e03b00c55bb6d9fa7460234a95ce5
unknown <frifqi50@gmail.com>
2023-05-31 22:13:47 +0700

    Halaman CRUD User

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('user', 'UserController');
-Route::resource('attendance', 'AttendanceController')->only(['index', 'show']);
+
+
 
